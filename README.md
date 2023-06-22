7bfa73eb3129cbfd159a706f7c2b156502f7d1cc
Igor Goncharov <igor@goncharov.dev>
2023-06-22 15:06:08 -0400

    Peewee proto is working

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
 # ORMs
 
-Data of as of 6/22/2023
+Data as of 6/22/2023
 
-| ORM         | URL                                      | Watch/Fork/Stars | Issues/PRs |
-|-------------|------------------------------------------|------------------|------------|
-| SqlModel    | https://github.com/tiangolo/sqlmodel     | 200/1.4k/10.1k   | 177/55     |
-| PeeWee      | https://github.com/coleifer/peewee       | 138/430/10k      | 0/0        |
-| SqlAlchemy  | https://github.com/sqlalchemy/sqlalchemy | 92/1.2k/7.4k     | 162/13     |
-| TortoiseORM | https://github.com/tortoise/tortoise-orm | 41/312/3.6k      | 414/43     |
+| ORM                                                     | Docs URL                                                     | Watch/Fork/Stars | Issues/PRs |
+|---------------------------------------------------------|--------------------------------------------------------------|------------------|------------|
+| [SqlModel](https://github.com/tiangolo/sqlmodel)        | https://sqlmodel.tiangolo.com                                | 200/1.4k/10.1k   | 177/55     |
+| [PeeWee](https://github.com/coleifer/peewee)            | https://docs.peewee-orm.com                                  | 138/430/10k      | 0/0        |
+| [SqlAlchemy](https://github.com/sqlalchemy/sqlalchemy)  | https://www.sqlalchemy.org                                   | 92/1.2k/7.4k     | 162/13     |
+| [TortoiseORM](https://github.com/tortoise/tortoise-orm) | https://tortoise.github.io                                   | 41/312/3.6k      | 414/43     |
+| [PonyORM](https://github.com/ponyorm/pony)              | https://github.com/ponyorm/pony                              | 85/238/3.3k      | 283/33     |
+| [DjangoORM](https://github.com/django/django)           | https://docs.djangoproject.com/en/stable/topics/db/models/   | 2.3k/29.4k/71.6  | NA/137     | 
 
+
+
+# SqlModel
+### Pros:
+- The same company that developed FastAPI
+- Supposed to be to migrate from SqlAlchemy
+### Cons:
+
+# PeeWee
+### Pros:
+### Cons:
+
+# SqlAlchemy
+### Pros:
+### Cons:
+
+# TortoiseORM
+### Pros:
+### Cons:
+
+# PonyORM
+### Pros:
+### Cons:
+
+# DjangoORM
+### Pros:
+- Widely used framework
+### Cons:
+- Have to use Django web framework
--- peewee_main.py
+++ peewee_main.py
@@ -1,5 +1,5 @@
 import uuid
-from peewee_orm.peewee_ud_db import *
+from peewee_orm.peewee_ukd_db import *
 from datetime import date, time, datetime, timedelta
 
 
@@ -23,63 +23,113 @@ def main():
                                              ConfirmationToken='ConfirmationToken', ConfirmationTokenTime=datetime.now(), EmailValidationStatus=True,
                                              RecoveryToken='RecoveryToken', RecoveryTokenTime=datetime.now())
         student1 = Student.create(FirstName='Igor', LastName='Goncharov', PhoneNumber='6463732444',
-                                            AddressLine1='Add', City='JC', State='NJ', PostalCode='07310', Country='USA', UserData=student1_user_data)
-        student2_user_data = UserData.create(Login='student1@goncharov.dev', PasswordHash='Hash', PasswordSalt='Salt', HashAlgo='HashAlgo',
+                                  AddressLine1='Add', City='JC', State='NJ', PostalCode='07310', Country='USA', UserData=student1_user_data)
+        student2_user_data = UserData.create(Login='student2@goncharov.dev', PasswordHash='Hash', PasswordSalt='Salt', HashAlgo='HashAlgo',
                                              ConfirmationToken='ConfirmationToken', ConfirmationTokenTime=datetime.now(), EmailValidationStatus=True,
                                              RecoveryToken='RecoveryToken', RecoveryTokenTime=datetime.now())
         student2 = Student.create(FirstName='Igor', LastName='Goncharov', PhoneNumber='6463732444',
-                                            AddressLine1='Add', City='JC', State='NJ', PostalCode='07310', Country='USA', UserData=student2_user_data)
+                                  AddressLine1='Add', City='JC', State='NJ', PostalCode='07310', Country='USA', UserData=student2_user_data)
         # Location
         location = Location.create(Url='https://zoom.us', LocationType='T', Tutor=teacher)
         # Class
         class_item = Class.create(Name='Music', Status='Active', ClassType='ClassType',
                                   StartDate=date(2023, 7, 1), EndScheduleType='Fixed',
                                   EndDate=date(2023, 7, 28), MakeupRequired=True, TrackPrepayment=True, Location=location)
-        # Slots
+        # Slots count=3
         slot1 = Slot.create(uid=uuid.uuid4(), DayOfWeek=1, StartTime=time(16, 0), Duration=60, Class=class_item)
         slot2 = Slot.create(uid=uuid.uuid4(), DayOfWeek=3, StartTime=time(17, 0), Duration=60, Class=class_item)
         slot3 = Slot.create(uid=uuid.uuid4(), DayOfWeek=5, StartTime=time(18, 0), Duration=60, Class=class_item)
         # Sessions
-        t = date.today()
-        t1 = t + timedelta(days=7)
-        session1 = Session.create(StartDateTime=datetime.combine(t, slot1.StartTime), Duration=slot1.Duration, Slot=slot1, Class=class_item)
-        session2 = Session.create(StartDateTime=datetime.combine(t, slot2.StartTime), Duration=slot2.Duration, Slot=slot2, Class=class_item)
-        session3 = Session.create(StartDateTime=datetime.combine(t, slot3.StartTime), Duration=slot3.Duration, Slot=slot3, Class=class_item)
+        teacher = date.today()
+        t1 = teacher + timedelta(days=7)
+        session1 = Session.create(StartDateTime=datetime.combine(teacher, slot1.StartTime), Duration=slot1.Duration, Slot=slot1, Class=class_item)
+        session2 = Session.create(StartDateTime=datetime.combine(teacher, slot2.StartTime), Duration=slot2.Duration, Slot=slot2, Class=class_item)
+        session3 = Session.create(StartDateTime=datetime.combine(teacher, slot3.StartTime), Duration=slot3.Duration, Slot=slot3, Class=class_item)
         session4 = Session.create(StartDateTime=datetime.combine(t1, slot1.StartTime), Duration=slot1.Duration, Slot=slot1, Class=class_item)
         session5 = Session.create(StartDateTime=datetime.combine(t1, slot2.StartTime), Duration=slot2.Duration, Slot=slot2, Class=class_item)
         session6 = Session.create(StartDateTime=datetime.combine(t1, slot3.StartTime), Duration=slot3.Duration, Slot=slot3, Class=class_item)
 
-        # Enrollment
         start = session1.StartDateTime
-        end = session6.StartDateTime+timedelta(minutes=session6.Duration)
-        student1_enrollment = Enrollment.create(StartDate=start, EndDate=end, Notes='', Student=student1, Class=class_item)
-        student2_enrollment = Enrollment.create(StartDate=start, EndDate=end, Notes='', Student=student2, Class=class_item)
+        end = session6.StartDateTime + timedelta(minutes=session6.Duration)
+    print(f'id = {user_data.id}')
 
-        check_in1 = CheckIn.create(CheckInStatus=CheckInStatus.Present, TimeStamp=datetime.now(), Student=student1, Session=session2)
-        check_in2 = CheckIn.create(CheckInStatus=CheckInStatus.Absent, TimeStamp=datetime.now(), Student=student2, Session=session2)
+    # Enrollment:
+    with db.atomic():
+        enroll_class = Class.get(Class.Name == 'Music')
+        enroll_student1 = UserData.get(UserData.Login == 'student1@goncharov.dev')
+        enroll_student2 = UserData.get(UserData.Login == 'student2@goncharov.dev')
 
-        # user_data.save()
-        # teacher.save()
-        # location.save()
-        # class_item.save()
-        print(f'id = {user_data.id}')
+        student1_enrollment = Enrollment.create(StartDate=start, EndDate=end, Notes='', Student=enroll_student1.Student, Class=enroll_class)
+        student1_enrollment.save()
+        student2_enrollment = Enrollment.create(StartDate=start, EndDate=end, Notes='', Student=enroll_student2.Student, Class=enroll_class)
+        student2_enrollment.save()
 
+    # CheckIns:
+    with db.atomic():
+        enroll_class = Class.get(Class.Name == 'Music')
+        enroll_student1 = UserData.get(UserData.Login == 'student1@goncharov.dev')
+        enroll_student2 = UserData.get(UserData.Login == 'student2@goncharov.dev')
+        session_1 = Session.select().where(Session.Class == enroll_class).order_by(Session.StartDateTime)[1]
+        check_in0 = CheckIn.create(CheckInStatus=CheckInStatus.Present, TimeStamp=datetime.now(), Student=enroll_student1.Student, Session=session_1)
+        check_in0.save()
+        check_in1 = CheckIn.create(CheckInStatus=CheckInStatus.Absent, TimeStamp=datetime.now(), Student=enroll_student2.Student, Session=session_1)
+        check_in1.save()
+
+        session_last = Session.select().where(Session.Class == enroll_class).order_by(Session.StartDateTime)[-1]
+        check_in2 = CheckIn.create(CheckInStatus=CheckInStatus.Present, TimeStamp=datetime.now(), Student=enroll_student1.Student, Session=session_last)
+        check_in2.save()
+        check_in3 = CheckIn.create(CheckInStatus=CheckInStatus.Absent, TimeStamp=datetime.now(), Student=enroll_student2.Student, Session=session_last)
+        check_in3.save()
+
+    # Output
     with db:
-        t = Teacher.get(Teacher.id == 1)
-        for location in t.Locations:
+        teacher = Teacher.get(Teacher.id == 1)
+        enrolled_student1 = UserData.get(UserData.Login == 'student1@goncharov.dev').Student
+        enrolled_student2 = UserData.get(UserData.Login == 'student2@goncharov.dev').Student
+
+        check_session_1 = Session.select().where(Session.Class == enroll_class).order_by(Session.StartDateTime)[1]
+        check_session_last = Session.select().where(Session.Class == enroll_class).order_by(Session.StartDateTime)[-1]
+
+        for location in teacher.Locations:
             print(f'{location.id} - {location.Url}')
-        for slot in Slot.select():
+
+        class_item = Class.get(Class.Name == 'Music')
+        slots = class_item.Slots
+        print(f'Count(Slots) = {slots.count()}')
+        assert slots.count() == 3
+        for slot in slots:
             print(f'{slot.uid} - {slot.StartTime}')
-        print(f'Len(Slots) = {len(Slot.select())}')
-        print(f'Len(Session) = {len(Session.select())}')
-        print(f'Len(Enrollment) = {len(Enrollment.select())}')
 
-        print(f'Len(CheckIn) = {len(CheckIn.select())}')
-        print(f'Len(CheckIn)==Present - {len(CheckIn.select().where(CheckIn.CheckInStatus==CheckInStatus.Present))}')
-        print(f'Len(CheckIn)==Absent - {len(CheckIn.select().where(CheckIn.CheckInStatus==CheckInStatus.Absent))}')
+        sessions = class_item.Sessions
+        print(f'Count(Session) = {sessions.count()}')
+        assert sessions.count() == 6
 
+        enrollments = class_item.Enrollments
+        print(f'Count(Enrollment) = {enrollments.count()}')
 
-    pass
+        all_checkins = CheckIn.select().count()
+        print(f'All Count(CheckIn) = {all_checkins}')
+        assert all_checkins == 4
+
+        present_count = CheckIn.select().join(Session).where(CheckIn.Session.Class == class_item)\
+            .where(CheckIn.Student == enrolled_student1).where(CheckIn.CheckInStatus == CheckInStatus.Present).count()
+        print(f'{class_item.Name} Count(CheckIn)==Present - {present_count}')
+        assert present_count == 2
+
+        present_count_session1 = CheckIn.select().where(CheckIn.Session == check_session_1) \
+            .where(CheckIn.Student == enrolled_student1).where(CheckIn.CheckInStatus == CheckInStatus.Present).count()
+        print(f'{class_item.Name} at {check_session_1.StartDateTime}+{check_session_1.Duration}m Count(CheckIn)==Present - {present_count_session1}')
+        assert present_count_session1 == 1
+
+        absent_count = CheckIn.select().join(Session).where(CheckIn.Session.Class == class_item)\
+            .where(CheckIn.Student == enrolled_student2).where(CheckIn.CheckInStatus == CheckInStatus.Absent).count()
+        print(f'{class_item.Name} Count(CheckIn)==Absent - {absent_count}')
+        assert absent_count == 2
+
+        absent_count_last_session = CheckIn.select().where(CheckIn.Session == check_session_last) \
+            .where(CheckIn.Student == enrolled_student1).where(CheckIn.CheckInStatus == CheckInStatus.Present).count()
+        print(f'{class_item.Name} at {check_session_last.StartDateTime}+{check_session_1.Duration}m Count(CheckIn)==Present - {absent_count_last_session}')
+        assert absent_count_last_session == 1
 
 
 if __name__ == '__main__':
