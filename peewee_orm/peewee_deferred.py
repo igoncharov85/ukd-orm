d01b9d6ca8a727137444658e564c2ca7217342da
Igor Goncharov <igor@goncharov.dev>
2023-09-08 20:00:35 -0400

    Latest update

--- peewee_orm/peewee_deferred.py
+++ peewee_orm/peewee_deferred.py
@@ -1,8 +1,27 @@
+import datetime
+import pytz
+
 import peewee
 from peewee import *
 
 db = SqliteDatabase(':memory:')
 print(peewee.__version__)
+
+
+class DateTimeFieldWithoutTZ(DateTimeField):
+    field_type = 'DATETIME'
+
+    def __remove_time_zone(self, value: datetime) -> datetime:
+        if value:
+            if value.tzinfo:
+                return value.replace(tzinfo=None)
+            return value
+
+    def db_value(self, value: datetime) -> datetime:
+        return self.__remove_time_zone(value)
+
+    def python_value(self, value: datetime) -> datetime:
+        return self.__remove_time_zone(value)
 
 
 class DbModel(Model):
@@ -13,6 +32,8 @@ class DbModel(Model):
 class UserData(DbModel):
     UserId = AutoField(primary_key=True)
     Login = CharField()
+    DateTimeWithTZ = DateTimeFieldWithoutTZ()
+    DateTimeNoTz = DateTimeField()
 
 
 class Teacher(DbModel):
@@ -33,14 +54,33 @@ def main():
     db.create_tables([UserData, Teacher, Student])
 
     with db.atomic():
-        user_data = UserData.create(Login='user_login')
+        #now_with_tz = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+        #assert now_with_tz.tzinfo is not None
+
+        now_with_tz = datetime.datetime.utcnow()
+
+        now_without_tz = datetime.datetime.utcnow()
+
+        user_data = UserData.create(Login='user_login', DateTimeWithTZ=now_with_tz, DateTimeNoTz=now_without_tz)
         teacher = Teacher.create(SchoolName='School1', UserData=user_data)
         student = Student.create(University='University1', UserData=user_data)
+        user_data.save()
+        teacher.save()
+        student.save()
 
     assert user_data.UserId == student.StudentId
     assert user_data.Student == student
     assert user_data.UserId == teacher.TeacherId
     assert user_data.Teacher == teacher
+
+    with db.atomic():
+        first = UserData.select().first()
+
+        print(f'{type(first.DateTimeNoTz)} - {first.DateTimeNoTz}')
+        assert first.DateTimeNoTz.tzinfo is None
+
+        print(f'{type(first.DateTimeWithTZ)} - {first.DateTimeWithTZ}')
+        #assert first.DateTimeWithTZ.tzinfo is not None
 
 
 if __name__ == '__main__':
--- peewee_orm/peewee_start_duration.py
+++ peewee_orm/peewee_start_duration.py
@@ -1,21 +1,49 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import peewee
 from peewee import *
+from playhouse.db_url import connect
+from playhouse.hybrid import hybrid_property
 
-db = SqliteDatabase(':memory:')
+#db = SqliteDatabase(':memory:')
+db = connect('mysql://')
 print(peewee.__version__)
 
 
-class Class(Model):
-    ClassId = AutoField(primary_key=True)
+class Session(Model):
+    SessionId = AutoField(primary_key=True)
 
     StartDateTime = DateTimeField()
     Duration = IntegerField()
 
+    @hybrid_property
+    def EndDateTime(self):
+        return self.StartDateTime + timedelta(minutes=self.Duration)
+
+    @EndDateTime.expression
+    def EndDateTime(cls):
+        end = SQL(f'DATE_ADD({cls.StartDateTime}, INTERVAL {cls.Duration} MINUTE)', [cls.StartDateTime, cls.Duration])
+        return end
+
+    class Meta:
+        database = db
+
+
+db.create_tables([Session])
+
 
 def main():
-    start = datetime
-    class0 = Class.create(StartDateTime=datetime.now())
+    now = datetime.now()
+    in_an_hour = now + timedelta(hours=1)
+    with db.atomic():
+        Session.create(StartDateTime=now, Duration=10)
+        Session.create(StartDateTime=now, Duration=10)
+        Session.create(StartDateTime=in_an_hour, Duration=10)
+    assert len(Session) == 3
+    for session in Session:
+        print(f'{session.SessionId}={session.Duration} - {session.StartDateTime} - {session.EndDateTime}')
+
+    for session in Session.select().where(Session.EndDateTime <= in_an_hour):
+        print(f'{session.SessionId} - {session.StartDateTime} - {session.EndDateTime}')
 
 
 if __name__ == '__main__':
